fce57955f666b81c377b9202ab34bb3146bf1246
emerson-acacio-fs <emerson.acacio2@gmail.com>
2022-09-13 21:59:31 -0300

    Testando a modificação no git

--- src/hooks/useTaskList.ts
+++ src/hooks/useTaskList.ts
@@ -1,6 +1,6 @@
 import {useContext} from 'react'
 import {ITasksContext, TasksContext} from '../context/TasksContext'
-
+// testar  o git
 export function useTaskList(): ITasksContext {
   const context = useContext(TasksContext)
   return context
